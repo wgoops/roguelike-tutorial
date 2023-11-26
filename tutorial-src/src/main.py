322456ff1bb0c0c19efed7084398d8bbfd437484
wgoops <shaine.quigley@gmail.com>
2023-11-26 17:23:38 -0500

    finished part 1 of tutorial

--- tutorial-src/src/main.py
+++ tutorial-src/src/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import tcod 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
 
 def main(): 
 
@@ -7,11 +9,18 @@ def main():
     screen_width = 80
     screen_height = 50
     
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
     ##import tileset
     
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )   #TODO make path to png not absolutely awful
+
+    ## set up event handler
+
+    event_handler = EventHandler()
     
     #define screen settings
     terminal = tcod.context.new_terminal(
@@ -28,10 +37,19 @@ def main():
         
         ## SET UP GAME LOOP, VERY IMPORTANT
         while True: 
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
             context.present(root_console)
+            root_console.clear()
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = event_handler.dispatch(event)
+                if action is None: 
+                    continue
+                
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+                
+                elif isinstance(aciton, EscapeAction):
                     raise SystemExit()
 
 if __name__ == "__main__":
